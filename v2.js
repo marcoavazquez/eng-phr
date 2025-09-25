dce13e6f3faeba6afdfd9700250ea0ac1777080d
Marco Antonio <marcoa.vazqueza@gmail.com>
2025-09-25 16:32:11 -0600

    udpates styles

--- v2.js
+++ v2.js
@@ -46,14 +46,14 @@ window.addEventListener('load',async function () {
     window.history.replaceState(null, null, '?' + searchParams.toString())
     const [english, spanish] = list[currentIndex].split('|')
     phraseContainer.textContent = currentEnglish = english.trim()
-    translationContainer.textContent = spanish || '🕸️' 
+    translationContainer.textContent = spanish || '[No translation]' 
 
-    const [prevEnglish, prevSpanish] = list[currentIndex - 1] ? list[currentIndex - 1].split('|') : ['','']
-    prevPhrases.textContent = prevEnglish
-    prevTranslation.textContent = prevSpanish || '🕸️'
-    const [nextEnglish, nextSpanish] = list[currentIndex + 1] ? list[currentIndex + 1].split('|') : ['','']
-    nextPhrases.textContent = nextEnglish
-    nextTranslation.textContent = nextSpanish || '🕸️'
+    // const [prevEnglish, prevSpanish] = list[currentIndex - 1] ? list[currentIndex - 1].split('|') : ['','']
+    // prevPhrases.textContent = prevEnglish
+    // prevTranslation.textContent = prevSpanish || '🕸️'
+    // const [nextEnglish, nextSpanish] = list[currentIndex + 1] ? list[currentIndex + 1].split('|') : ['','']
+    // nextPhrases.textContent = nextEnglish
+    // nextTranslation.textContent = nextSpanish || '🕸️'
     setCounter()
   }
 
